2f3f20726adafb42735ae4e53422bc7b37e9595c
Hugo <hugo.deoliveira.legacy@gmail.com>
2026-08-13 10:01:37 -0300

    Atualização em extração de dados por carro para manter o estado classe request e correção de bug extração de pilotos

--- extrator/extractor_car_data.py
+++ extrator/extractor_car_data.py
@@ -2,12 +2,14 @@ from Request.OpenF1_Client import RequestFactory as rf
 
 endpoint_car_data = "/car_data"
 
-class ExtractorCarData():
-    def __init__(self, ):
+class ExtractorCarData:
+    def __init__(self):
         self.client = rf(endpoint_car_data)
-       
+        
     
-    def extractCarData(self, driver_number):
-        response = self.client.get_data(self.client.params+driver_number, endpoint_car_data)
+    def extractCarData(self, driver_number: int) -> list:
+        params = self.client.params.copy()
+        params['driver_number'] = driver_number   
+        response = self.client.get_data(params, endpoint_car_data)
         print(response)
         return response
--- extrator/extractor_driver.py
+++ extrator/extractor_driver.py
@@ -1,16 +1,20 @@
 from Request.OpenF1_Client import RequestFactory as rf
 import extractor_car_data
+
 endpointDrivers = "/drivers"
+get_data_car_data = extractor_car_data.ExtractorCarData()
 class ExtractorDriver:
     def __init__(self):
         self.client = rf(endpointDrivers)
     
-    def extractDrivers(self):
+    def extractDrivers(self) -> list:
         driver_number = []
         response = self.client.get_data(self.client.params, endpointDrivers)
-        for i in response['driver_number']:
+        for i in response:
             number = i['driver_number']
             driver_number.append(number)
-            getCarData = extractor_car_data.ExtractorCarData(number)
+            getCarData = get_data_car_data.extractCarData(int(number))
         return response
 
+    
+print(ExtractorDriver().extractDrivers())
