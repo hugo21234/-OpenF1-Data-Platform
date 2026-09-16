c2a612fc191841a7b9b8bceba61a7ed3626d8796
hugo.deoliveira.legacy@gmail.com <hugo.deoliveira.legacy@gmail.com>
2026-09-16 22:03:10 +0000

    alteração sql race controlo e create table , lineage gambiarrado

--- sql/bronze/create_table_drivers.sql
+++ sql/bronze/create_table_drivers.sql
@@ -1,14 +1,16 @@
-CREATE TABLE drivers (
-    broadcast_name STRING,
-    country_code INT,
+DROP TABLE IF EXISTS f1_plataform_data.bronze.drivers;
+
+CREATE TABLE f1_plataform_data.bronze.drivers (
+    meeting_key INT,
+    session_key INT,
     driver_number BIGINT,
+    broadcast_name STRING,
+    full_name STRING,
+    name_acronym STRING,
+    team_name STRING,
+    team_colour STRING,
     first_name STRING,
     last_name STRING,
     headshot_url STRING,
-    full_name STRING,
-    meeting_key INT,
-    session_key INT,
-    name_acronym STRING,
-    team_colour STRING,
-    team_name STRING
+    country_code STRING
 )
